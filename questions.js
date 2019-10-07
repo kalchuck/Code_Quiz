5fddf6dc279327aff27568f43c910c630234a8de
Kalyan Gadda <kalchuck@gmail.com>
2019-10-06 18:05:28 -0600

    updated questions for quiz

---
+++ questions.js
@@ -0,0 +1,48 @@
+var questions = [
+    {
+      title: "What does HTML stand for?:",
+      choices: ["Hyper Text Markup Language", "Hyperlinks and Text Markup Language", "Home Tool Markup Language", "numbers"],
+      answer: "Hyper Text Markup Language"
+    },
+    {
+      title: "What is the correct sequence of HTML tags for starting a webpage?",
+      choices: ["Br", "Break", "Lb", "bl"],
+      answer: "Br"
+    },
+    {
+        title: "Choose the correct HTML tag to make text bold?",
+        choices: ["Bold", "B", "Bld", "Br"],
+        answer: "Bold"
+    },
+    {
+        title: "You're 3rd place right now in a race. What place are you in when you pass the person in 2nd place?",
+        choices: ["1st", "2nd", "3rd", "None of the above"],
+        answer: "2nd"
+    },
+    {
+        title: "How many months have 28 days?",
+        choices: ["2", "1", "All of them", "Depends if there's a leap year or not."],
+        answer: "1"
+    },
+    {
+        title: "what is the capital of USA",
+        choices: ["Washington DC", "New York", "Washington", "Texas"],
+        answer: "Washington DC"
+    },
+    {
+        title: "How many colours in ",
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses"
+    },
+    {
+        title: "The condition in an if / else statement is enclosed within ____.",
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses"
+    },
+    {
+        title: "The condition in an if / else statement is enclosed within ____.",
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses"
+    },
+  ];
+  
